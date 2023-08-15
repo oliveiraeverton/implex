2a76d01850f6a3f9d90d0fe8124f1f2f4ba79016
Everton de Oliveira <everton.oliveira@ufms.br>
2023-08-15 09:04:31 -0400

    feat(main): incrementando a lógica de repetição

--- main.c
+++ main.c
@@ -27,87 +27,85 @@ void imprimeVetor(int arr[], int n){
 
 }
 
-//Essa função não está sendo utilizada
-void CriaVetoComElementosRandomicos(int **arr, int *n){
-    // Inicializar o gerador de números randômicos com um valor de semente
-    srand(time(NULL));
-    *n = 5000;
-    // Alocar memória para o vetor
-    *arr = (int *)malloc(*n * sizeof(int));
-
-    // Variável *vetor guarda a posição inicial do vetor alocado
-    int *vetor = *arr;
-    // Preencher o vetor com elementos randômicos
-    for (int i = 0; i < *n; i++) {
-        
-        **arr = rand() % 1000; // Gera um número randômico entre 0 e 99
-        (*arr)++;
-    }
-    // Recupera a posição do vetor inicial
-    *arr = vetor;
-}
-
 //Função para criar os números aleatórios
-void CriaVetoComElementosRandomicos2(int **arr, int *n){
+void CriaVetorComElementosRandomicos(int **vetorParaOrdenacao, int **vetorOriginal,int *n){
     // Inicializar o gerador de números randômicos com um valor de semente
     srand(time(NULL));
-    *n = 500;
+    //*n = 500;
     // Alocar memória para o vetor
-    *arr = (int *)malloc(*n * sizeof(int));
-
-    // Variável *vetor guarda a posição inicial do vetor alocado
-    //int *vetor = *arr;
+    *vetorParaOrdenacao = (int *)malloc(*n * sizeof(int));
+    *vetorOriginal = (int *)malloc(*n * sizeof(int));
+    
     // Preencher o vetor com elementos randômicos
-    int *pointF = *arr+*n;
-    for (int *i = *arr; i < pointF; i++) {
+    int *k = *vetorOriginal;
+    int *pointF = *vetorParaOrdenacao+*n;
+    for (int *i = *vetorParaOrdenacao; i < pointF; i++) {
 
         *i = rand() % 1000; // Gera um número randômico entre 0 e 99
-        
+        *k = *i;
+        k++;
     }
+    
 
 }
 
 
-
-void CopiarElementosDoVetor(int *vetor,int *vetorC ,int tamanhoDoVetor){
-    for(int i = 0; i < tamanhoDoVetor; i++){
-        vetorC[i] = vetor[i];
+void CopiarElementosDoVetor(int *vetorOriginal,int **vetorCopia ,int tamanhoDoVetor){
+    // vetorCopia é o vetor que vai receber os elementos originais do vetorOriginal
+    for(int *i = *vetorCopia; i < *vetorCopia + tamanhoDoVetor; i++){
+        *i = *vetorOriginal++;
+    
     }
 }
 
 int main(){
 
-    clock_t start_time, end_time;
-    double elapsed_time;
-    double calculoDeTempo = 0;
-    int tamanhoDoVetor=0;
+    //******************************************   DECLARAÇÃO DE VARIÁVEIS ******************************************//
+    clock_t start_time, end_time;   // Variável para iniciar e terminar o cronômetro
+    double elapsed_time;            // Variável que guardará a diferença entre inicío e fim do cronômetro
+    double calculoDeTempo = 0;      // variável para calcular a média do tempo
+    int tamanhoDoVetor=5;         // Variável que irá controlar o tamanho do vetor
+    int *vetor;                     //Vetor utilizado para a ordenação dentro das funções de ordenação
+    int *vetorOriginal;             // Vetor que terá as posições originais de cada elemento
+    int inc = 1000;    // Tamanho Inicial
+    int fim = 10000000;    // Tamanho Final
+    int stp = 100000;    // Intervalo entre os Tamanhos
+    int rpt = fim/stp;    // Número de repetições a serem realizadas
+    //***************************************************************************************************************//
+
+      
+   
+    //******************************************   DEBBUGUER ******************************************//
+    //imprimeVetor(vetor, tamanhoDoVetor);
+    //imprimeVetor(vetorOriginal, tamanhoDoVetor);
+    //CopiarElementosDoVetor(vetor, &vetorOriginal, tamanhoDoVetor);
+    //******************************************   DEBBUGUER ******************************************//
     
+    
+    for(int w = 0; w < rpt; w++ ){
+        printf("Tamanho do Vetor > %d\n", inc);
+        // Alimenta o vetor com números Randômicos
+        CriaVetorComElementosRandomicos(&vetor, &vetorOriginal, &inc);
+        inc += stp;
+        for(int k = 0; k < 10; k++){
+            // Iniciar a contagem de tempo
+            start_time = clock();
+            insertionSort(vetor, tamanhoDoVetor);
+            // Parar a contagem de tempo
+            end_time = clock();
+            // Calcular o tempo decorrido em segundos
+            elapsed_time = ((double)(end_time - start_time)) / CLOCKS_PER_SEC;
+            calculoDeTempo += elapsed_time;
+            CopiarElementosDoVetor(vetorOriginal, &vetor, tamanhoDoVetor);
+            
 
-    int *vetor;
-
-    CriaVetoComElementosRandomicos2(&vetor, &tamanhoDoVetor);
-
-
-    int vetorOriginal[tamanhoDoVetor];
-    CopiarElementosDoVetor(vetor, vetorOriginal, tamanhoDoVetor);
-
-    for(int k = 0; k < 10; k++){
-        // Iniciar a contagem de tempo
-        start_time = clock();
-        insertionSort(vetor, tamanhoDoVetor);
-        // Parar a contagem de tempo
-        end_time = clock();
-        printf("Tempo decorrido: %.6f segundos\n", elapsed_time);
-        // Calcular o tempo decorrido em segundos
-        elapsed_time = ((double)(end_time - start_time)) / CLOCKS_PER_SEC;
-        calculoDeTempo += elapsed_time;
-
-        CopiarElementosDoVetor(vetorOriginal, vetor, tamanhoDoVetor);
-
+        }
+        printf("Tempo decorrido media final: %.6f segundos\n", calculoDeTempo/10);
+        free(vetor);
+        free(vetorOriginal);
     }
-    printf("AQUI\n");
-    printf("Tempo decorrido: %.6f segundos\n", calculoDeTempo/10);
-
-    free(vetor);
+        
+    //printf("AQUI\n");
+    
     return 0;
 }
